bfd638fb8284fcf6790dc9828b644613a36a0702
Mohamed LEBIB <moh.lam9596@gmail.com>
2017-11-30 22:56:32 +0100

    V1 projet
    Codage du 1er sprint.

---
+++ Code/Workspace/project/src/main/java/com/example/project/entities/User.java
@@ -0,0 +1,73 @@
+package com.example.project.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String nom, prenom, email, password;
+	
+	private String passwordConfirm;
+	String dateInscription =  new Date().toString();
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+	
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+	public String getDateInscription() {
+		return dateInscription;
+	}
+	public void setDateInscription(String dateInscription) {
+		this.dateInscription = dateInscription;
+	}
+}
---
+++ Code/Workspace/project/src/main/java/com/example/project/service/BlogService.java
@@ -0,0 +1,20 @@
+package com.example.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.project.entities.User;
+import com.example.project.repositories.UserRepository;
+
+@Service
+public class BlogService implements BlogServiceInterface {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public void addUser(User user) {
+		userRepository.save(user);
+	}
+
+}
