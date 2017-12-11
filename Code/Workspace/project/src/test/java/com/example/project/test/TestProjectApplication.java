0d2c45ba4a600b72e314b7182ccd68bf7f58d330
MarinaStark <marina.passoubady@gmail.com>
2017-12-12 00:31:33 +0100

    amelioration dette sonar, debut mep des tests'

--- Code/Workspace/project/src/test/java/com/example/project/test/TestProjectApplication.java
+++ Code/Workspace/project/src/test/java/com/example/project/test/TestProjectApplication.java
@@ -1,22 +1,10 @@
 package com.example.project.test;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.example.project.ProjectApplication;
 
