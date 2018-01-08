287ad1712e19b1f892ec9c36597febfa27796d23
Mohamed LEBIB <moh.lam9596@gmail.com>
2018-01-08 13:59:03 +0100

    IT.Smells

--- src/main/java/com/example/project/controllers/Signin.java
+++ src/main/java/com/example/project/controllers/Signin.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
