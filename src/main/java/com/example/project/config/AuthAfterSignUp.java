dd6558515a9b78dc22a6f0526fe05a3914c923dc
Mohamed LEBIB <moh.lam9596@gmail.com>
2018-01-08 17:54:45 +0100

    Ultime coup de Javel
    ça sent bon la rose désormais :)

--- src/main/java/com/example/project/config/AuthAfterSignUp.java
+++ src/main/java/com/example/project/config/AuthAfterSignUp.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -19,6 +20,7 @@ public class AuthAfterSignUp implements AuthenticationProvider {
 	public Authentication authenticate(Authentication auth) throws AuthenticationException {
 		 
 		User user = (User) auth.getDetails(); 
+		if (user == null) throw new AuthenticationServiceException("Une erreur est survenue lors de l'authentification ! Veuillez réessayer.");
 		return new UsernamePasswordAuthenticationToken
 	            (user.getEmail(), user.getPassword(), Collections.emptyList());
 	}
